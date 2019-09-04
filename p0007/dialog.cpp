83fc8425aa84443590d86a7b65a3dcc7d7a0623f
comptonben <comptonben504@gmail.com>
2019-09-04 18:13:57 -0500

    practice problem 0007

---
+++ p0007/dialog.cpp
@@ -0,0 +1,21 @@
+#include "dialog.h"
+#include "ui_dialog.h"
+
+#include <QSlider>
+
+Dialog::Dialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::Dialog)
+{
+    ui->setupUi(this);
+
+    connect(ui->verticalSlider, &QSlider::valueChanged,
+            ui->horizontalSlider, &QSlider::setValue);
+    connect(ui->horizontalSlider, &QSlider::valueChanged,
+            ui->verticalSlider, &QSlider::setValue);
+}
+
+Dialog::~Dialog()
+{
+    delete ui;
+}
