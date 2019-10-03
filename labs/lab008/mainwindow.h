8cf6b40f11412d9f4d97a5354947405ed74036a0
Ben Compton <comptonben504@gmail.com>
2019-10-03 16:34:52 -0500

    lab008

--- labs/lab008/mainwindow.h
+++ labs/lab008/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QStandardItemModel>
 
 namespace Ui {
 class MainWindow;
@@ -17,6 +18,7 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    QStandardItemModel *itemModel = new QStandardItemModel();
 
 private slots:
     void addStuff();
